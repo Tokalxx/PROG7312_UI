c28f6a6aea8c03a01945dad96667a340f97b4ed1
Tokal <SeanSMaseola@gmail.com>
2023-11-08 20:24:49 +0200

    Cmmit01: 08/11/2023 - Trying to fix issue with displaying uassign.

---
+++ Core/FindingCallNumber.cs
@@ -0,0 +1,117 @@
+﻿using PROG7312_UI.DataTree;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PROG7312_UI.Core
+{
+    public class FindingCallNumber
+    {
+        private static readonly FindingCallNumber _instance = new FindingCallNumber();
+        string filePath = "C:\\Users\\tokal\\Desktop\\ToBeBetter\\Learning Projects\\10. C# Tutorial Files\\3. ASP.NET\\PROG7312_UI\\TextFile\\DDS.txt";
+        CallNumberTree cnt = new CallNumberTree(new CallNumberNode("0", ".", null));
+        Random ran1 = new Random();
+
+
+        private FindingCallNumber()
+        {
+            genTree(filePath, cnt);
+        }
+
+        public static FindingCallNumber GetInstance()
+        {
+            return _instance;
+        }
+
+        public CallNumberTree GetTree()
+        {
+            return cnt;
+        }
+
+
+        public List<CallNumberNode> genQuestion1(CallNumberNode node)
+        {
+            int level1 = Convert.ToInt32(node.Parent.Parent.Num.Substring(0, 1));
+            CallNumberNode tempCallNum = cnt.Root.Children[level1];
+            List<CallNumberNode> tempList = new List<CallNumberNode>();
+            tempList.Add(tempCallNum);
+
+            for (int i = 0; i < 3; ++i)
+            {
+                CallNumberNode tempAddValue = cnt.Root.Children[ran1.Next(0, 10)];
+                if (tempAddValue != tempCallNum && !tempList.Contains(tempAddValue))
+                {
+                    tempList.Add(tempAddValue);
+                }
+                else
+                {
+                    --i;
+                }
+            }
+            return tempList;
+        }
+
+        public List<CallNumberNode> genQuestion2(CallNumberNode node)
+        {
+            int level1 = Convert.ToInt32(node.Parent.Parent.Num.Substring(0, 1));
+            int level2 = Convert.ToInt32(node.Parent.Num.Substring(1, 1));
+            CallNumberNode tempCallNum = cnt.Root.Children[level1].Children[level2];
+            List<CallNumberNode> tempList = new List<CallNumberNode>();
+            tempList.Add(tempCallNum);
+
+            for (int i = 0; i < 3; ++i)
+            {
+                CallNumberNode tempAddValue = cnt.Root.Children[level1].Children[ran1.Next(0, 9)];
+                if (tempAddValue != tempCallNum && !tempList.Contains(tempAddValue))
+                {
+                    tempList.Add(tempAddValue);
+                }
+                else
+                {
+                    --i;
+                }
+            }
+            return tempList;
+        }
+
+
+
+        public static void genTree(string filePath, CallNumberTree tr)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                string[] subline;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int level1 = 0;
+                    int level2 = 0;
+
+                    if (line.Contains(" - "))
+                    {
+                        subline = line.Split(new[] { '-' }, 2);
+                        tr.Root.addNode(subline[0], subline[1]);
+                    }
+                    else
+                    {
+                        subline = line.Split(new[] { ',' }, 2);
+                        level1 = Convert.ToInt32(subline[0].Substring(0, 1));
+                        int callNumber = Convert.ToInt32(subline[0]);
+
+                        if (callNumber % 10 == 0)
+                        {
+                            tr.Root.Children[level1].addNode(subline[0], subline[1]);
+                        }
+                        else
+                        {
+                            level2 = Convert.ToInt32(subline[0].Substring(1, 1));
+
+                            tr.Root.Children[level1].Children[level2].addNode(subline[0], subline[1]);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
--- MVVM/View/FindingCallNumbersView.xaml.cs
+++ MVVM/View/FindingCallNumbersView.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using PROG7312_UI.Core;
+using PROG7312_UI.DataTree;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PROG7312_UI.MVVM.View
 {
@@ -20,9 +13,113 @@ namespace PROG7312_UI.MVVM.View
     /// </summary>
     public partial class FindingCallNumbersView : UserControl
     {
+        List<RadioButton> multipleQuestion = new List<RadioButton>();
+        FindingCallNumber cnObject = FindingCallNumber.GetInstance();
+        CallNumberNode answerLevel3;
+        CallNumberTree treeObject;
+        static CallNumberNode answerValue;
+        CallNumberNode callNum = null;
+        bool check1 = true;
+        int Level = 1;
+        Random ran = new Random();
+
+
         public FindingCallNumbersView()
         {
             InitializeComponent();
+            treeObject = cnObject.GetTree();
+
         }
+
+        private void buttonAnswer_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string answer2 = answerLevel3.Parent.Num + " " + answerLevel3.Parent.Des;
+            string answer3 = answerLevel3.Parent.Parent.Num + " " + answerLevel3.Parent.Parent.Des;
+
+            if (Level == 2)
+            {
+                foreach (RadioButton x in stackPanelMultiChoice.Children)
+                {
+                    if (x.IsChecked == true)
+                    {
+                        if (x.Content.ToString() == answer3)
+                        {
+                            MessageBox.Show("Correct");
+                            generateCallNumbers();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Wrong");
+                            Level = 1;
+                            generateCallNumbers();
+
+                        }
+                    }
+                }
+            }
+            else if (Level == 1)
+            {
+                foreach (RadioButton x in stackPanelMultiChoice.Children)
+                {
+                    if (x.IsChecked == true)
+                    {
+                        if (x.Content.ToString() == answer2)
+                        {
+                            MessageBox.Show("Correct");
+                            generateCallNumbers();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Wrong");
+                            Level = 1;
+                            generateCallNumbers();
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void buttonGen_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            generateCallNumbers();
+        }
+
+
+        public void generateCallNumbers()
+        {
+            if (Level == 1)
+            {
+                answerLevel3 = treeObject.Root.Children[ran.Next(0, 10)].Children[ran.Next(0, 1)].Children[ran.Next(0, 9)];
+                textBlockRootQuestion.Text = answerLevel3.Num + " " + answerLevel3.Des;
+
+                List<CallNumberNode> tempList = cnObject.genQuestion1(answerLevel3);
+
+                List<CallNumberNode> level1List = tempList.OrderBy(x => ran.Next()).ToList();
+
+                for (int i = 0; i < level1List.Count; ++i)
+                {
+                    ((RadioButton)stackPanelMultiChoice.Children[i]).Content = level1List[i].Num + " " + level1List[i].Des;
+                }
+
+                Level++;
+            }
+            else if (Level == 2)
+            {
+                List<CallNumberNode> tempList = cnObject.genQuestion2(answerLevel3);
+
+                List<CallNumberNode> level2List = tempList.OrderBy(x => ran.Next()).ToList();
+
+
+                for (int i = 0; i < level2List.Count; ++i)
+                {
+                    ((RadioButton)stackPanelMultiChoice.Children[i]).Content = level2List[i].Num + " " + level2List[i].Des;
+                }
+
+                Level = 1;
+            }
+        }
+
     }
 }
